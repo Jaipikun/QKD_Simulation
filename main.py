8d0fab1a25b36b534717ec62f09d0d7acccc9d93
Jaipikun <maska989@gmail.com>
2024-05-31 14:21:04 +0200

    Batteries of tests for auto sim and plot of config

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from numpy import average
 import matplotlib.pyplot as plt
 
 CONFIG = {
-    "key_lengths" : [15],
+    "key_lengths" : [10,15,20],
     "detector_efficiencies" : [i*5 for i in range(21)],
     "emitter_efficiencies" : [i*5 for i in range(21)],
-    "eavesdropping" : [True,False],
-    "tactic" : [0,1,2,3,4],
+    "eavesdropping" : [False,True],
+    "eve_tactic" : [0,1,2,3,4],
     "generate_plots" : True,
-    "number_of_iterations_per_simulation" : 5,
-    "change_eve_efficiency" : False # Not implemented
+    "number_of_iterations_per_simulation" : 200,
 }
 
 def set_of_simulations():
@@ -46,28 +45,101 @@ def set_of_simulations():
     
     return results_list
 
+def detector_efficiency_tests():
+    """
+    This function performs simulations for all given detector efficiencies
+    """
+    no_iterations = CONFIG["number_of_iterations_per_simulation"]
+    total_iter = no_iterations * len(CONFIG["emitter_efficiencies"])*len(CONFIG["eavesdropping"])*len(CONFIG["key_lengths"])
+    print(f"Simulation for detector efficiency : total number of iterations = {total_iter}")
+    iteration = 1
+    for key_length in CONFIG["key_lengths"]:
+        x_list = []
+        y_lists = [[],[],[],[]]
+        for eavesdropper in CONFIG["eavesdropping"]:
+            for det_eff in CONFIG["detector_efficiencies"]:
+                temp_result = [[],[]]
+                for j in range(no_iterations):
+                    print(f"Simulating {iteration} / {total_iter} with config {key_length,det_eff,eavesdropper}")
+                    alice_config = Simulation.create_configuration_dict(emitter_efficiency=100)
+                    bob_config = Simulation.create_configuration_dict(detector_efficiency=det_eff)
+                    eve_config = Simulation.create_configuration_dict(100,100,eavesdropper)
+                    sim = Simulation(alice_config,bob_config,eve_config)
+                    results_dict = sim.simulate(key_length,0)
+                    temp_result[0].append(results_dict["error_rate"])
+                    temp_result[1].append(len(results_dict["final_key"]))
+                    iteration+=1
+                x_list.append(det_eff)
+                y_lists[0].append(average(temp_result[0]))
+                y_lists[1].append(mode(temp_result[0])[0])
+                y_lists[2].append(average(temp_result[1]))
+                y_lists[3].append(mode(temp_result[1])[0])
+        fig,axes = plt.subplots(2,2,figsize=(20,20))
+        for k in range(4):
+            axes[k%2, int(k/2.0)].plot(x_list[0:len(CONFIG["detector_efficiencies"])],y_lists[k][0:len(CONFIG["detector_efficiencies"])],color = 'blue',label="without Eve")
+            axes[k%2, int(k/2.0)].plot(x_list[len(CONFIG["detector_efficiencies"]):-1],y_lists[k][len(CONFIG["detector_efficiencies"]):-1],'--r',label="with Eve")
+            axes[k%2, int(k/2.0)].legend(fontsize=12,loc='best')
+            axes[k%2, int(k/2.0)].set_title(f"{'Mode' if k%2 else 'Average'} {'quantum bit error rate' if k in [0,1] else 'usable key length'}",fontsize=20)
+            axes[k%2, int(k/2.0)].set_ylabel(f"{'quantum bit error rate' if k in [0,1] else 'usable key length'}",fontsize=20)
+            axes[k%2, int(k/2.0)].set_xlabel('Detector efficiency [%]',fontsize=20)
+        fig.savefig(f"test_detector_{key_length}.png")
+        print("xddd")
+
+
+def emitter_efficiency_tests():
+    """
+    This function performs simulations for all given emitter efficiencies
+    """
+    no_iterations = CONFIG["number_of_iterations_per_simulation"]
+    total_iter = no_iterations * len(CONFIG["emitter_efficiencies"])*len(CONFIG["eavesdropping"])*len(CONFIG["key_lengths"])
+    print(f"Simulation for emitter efficiency : total number of iterations = {total_iter}")
+    iteration = 1
+    for key_length in CONFIG["key_lengths"]:
+        x_list = []
+        y_lists = [[],[],[],[]]
+        for eavesdropper in CONFIG["eavesdropping"]:
+            for emi_eff in CONFIG["emitter_efficiencies"]:
+                temp_result = [[],[]]
+                for j in range(no_iterations):
+                    print(f"Simulating {iteration} / {total_iter} with config {key_length,emi_eff,eavesdropper}")
+                    alice_config = Simulation.create_configuration_dict(emitter_efficiency=emi_eff)
+                    bob_config = Simulation.create_configuration_dict(detector_efficiency=100)
+                    eve_config = Simulation.create_configuration_dict(100,100,eavesdropper)
+                    sim = Simulation(alice_config,bob_config,eve_config)
+                    results_dict = sim.simulate(key_length,0)
+                    temp_result[0].append(results_dict["error_rate"])
+                    temp_result[1].append(len(results_dict["final_key"]))
+                    iteration+=1
+                x_list.append(emi_eff)
+                y_lists[0].append(average(temp_result[0]))
+                y_lists[1].append(mode(temp_result[0])[0])
+                y_lists[2].append(average(temp_result[1]))
+                y_lists[3].append(mode(temp_result[1])[0])
+        fig,axes = plt.subplots(2,2,figsize=(20,20))
+        for k in range(4):
+            axes[k%2, int(k/2.0)].plot(x_list[0:len(CONFIG["emitter_efficiencies"])],y_lists[k][0:len(CONFIG["emitter_efficiencies"])],color = 'blue',label="without Eve")
+            axes[k%2, int(k/2.0)].plot(x_list[len(CONFIG["emitter_efficiencies"]):-1],y_lists[k][len(CONFIG["emitter_efficiencies"]):-1],'--r',label="with Eve")
+            axes[k%2, int(k/2.0)].legend(fontsize=12,loc='best')
+            axes[k%2, int(k/2.0)].set_title(f"{'Average' if k%2 else 'Mode'} {'quantum bit error rate' if k in [0,1] else 'usable key length'}",fontsize=20)
+            axes[k%2, int(k/2.0)].set_ylabel(f"{'quantum bit error rate' if k in [0,1] else 'usable key length'}",fontsize=20)
+            axes[k%2, int(k/2.0)].set_xlabel('Emitter efficiency [%]',fontsize=20)
+        fig.savefig(f"test_emitter_{key_length}.png")
+
+
+def eve_tactics_test():
+    """
+    This function performs simulations for all given detector efficiencies
+    """
+    no_iterations = CONFIG["number_of_iterations_per_simulation"]
+    total_iter = no_iterations * len(CONFIG["tactic"])*len(CONFIG["eavesdropping"])*len(CONFIG["key_lengths"])
+    print(f"Simulation for different eve tactics : total number of iterations = {total_iter}")
+    iteration = 1
+    # TBD
+
 def main():
-    results = set_of_simulations()
-    # TEMPORARY SOLUTION - TESTING PLOTS
-    temp = [[],[],[],[],[],[]]
-    for result in results:
-        temp[0].append(result["detector_efficiency"])
-        temp[1].append(result["emitter_efficiency"])
-        temp[2].append(result["average_error_rate"])
-        temp[3].append(result["mode_error_rate"])
-        temp[4].append(result["average_key_length"])
-        temp[5].append(result["mode_key_length"])
-    
-    fig = plt.figure(figsize=(12,8))
-    plt.plot(temp[1],temp[2],color="blue",label = "average qber")
-    plt.plot(temp[1],temp[3],color="red",label = "mode qber")
-    plt.legend()
-    fig.savefig("test_qber.png")
-    fig = plt.figure(figsize=(12,8))
-    plt.plot(temp[1],temp[4],color="blue",label = "average kl")
-    plt.plot(temp[1],temp[5],color="red",label = "mode kl")
-    plt.legend()
-    fig.savefig("test_kl.png")
+    detector_efficiency_tests()
+    print("\n\nDETECTOR EFFICIENCY SIMULATIONS FINISHED\n\n")
+    emitter_efficiency_tests()
     
 
 if __name__ == "__main__":
